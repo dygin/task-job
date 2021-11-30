1acee13e17cd4ac0f692fa01f953073f7f23f14e
永军 <dengyongjun@xinyuanstu.com>
2021-11-30 18:03:23 +0800

    更新

--- src/TaskJob.php
+++ src/TaskJob.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Clygin\TaskJob;
 
 class TaskJob
@@ -9,58 +10,67 @@ class TaskJob
 
     private $task = [];
 
-    public function __construct($childProcessesQuantity = 10)
+    private $parentProcesses = [];
+
+    public function __construct($childMaxProcessesQuantity = 10)
     {
-        $this->childProcessesQuantity = $childProcessesQuantity;
+        $this->childProcessesQuantity = $childMaxProcessesQuantity;
     }
 
-    public function addTask($task,$args = [])
+    public function addTask($task, $args = [])
     {
-        $this->task[] = ['callback' => $task,'args' => $args];
+        $this->task[] = ['callback' => $task, 'args' => $args];
         return $this;
     }
 
     public function exec()
     {
+        pcntl_async_signals(true);
+
+        pcntl_signal(SIGCHLD, SIG_IGN);
+
+        $rest_num = count($this->task);
+
+        if ($rest_num > $this->childProcessesQuantity) {
+            return false;
+        }
+
         foreach ($this->task as $task) {
             $callback = $task['callback'];
             $args     = $task['args'];
             $callbackSign = md5(json_encode($callback));
-            $sign         = false;
             if (!isset($this->childProcesses[$callbackSign])) {
                 $this->childProcesses[$callbackSign] = [];
             }
-            while (!$sign) {
-                while ($signalPid = pcntl_waitpid(-1,$status,WNOHANG)) {
-                    if ($signalPid == -1) {
-                        $this->childProcesses[$callbackSign] = array();
-                        break;
-                    } else {
-                        unset($this->childProcesses[$callbackSign][$signalPid]);
-                    }
-                }
-
-                if (count($this->childProcesses[$callbackSign]) < $this->childProcessesQuantity) {
-                    $pid = pcntl_fork();
-                    if ($pid == -1) {
-                        throw new \RuntimeException("fork失败了");
-                    } elseif ($pid) {
-                        //父进程
-                        $this->childProcesses[$callbackSign][$pid] = true;
-                        $sign = true;
-                    } else {
-                        //子进程处理
-                        if ($args) {
-                            call_user_func_array($callback, $args);
-                        } else {
-                            call_user_func($callback);
-                        }
-                        //执行完就退出 预防造成僵尸进程
-                        exit;
+            if (count($this->childProcesses[$callbackSign]) < $this->childProcessesQuantity) {
+                $pid = pcntl_fork();
+                if ($pid == -1) {
+                    throw new \RuntimeException("fork失败了");
+                } elseif ($pid) {
+                    $rest_num--;
+                    $this->childProcesses[$callbackSign][$pid] = true;
+                    if (empty($this->parentProcesses[posix_getpid()])) {
+                        $this->parentProcesses[posix_getpid()] = true;
                     }
                 } else {
-                    sleep(1);
+                    if ($args) {
+                        call_user_func_array($callback, $args);
+                    } else {
+                        call_user_func($callback);
+                    }
+                    if (!empty($this->parentProcesses) && $rest_num <= 1) {
+                        foreach ($this->parentProcesses as $parentProcesses => $rs) {
+                            posix_kill($parentProcesses, SIGKILL);
+                            pcntl_signal_dispatch();
+                            unset($this->parentProcesses[$parentProcesses]);
+                        }
+                    }
+                    posix_kill(posix_getpid(), SIGKILL);
+                    pcntl_signal_dispatch();
+                    exit;
                 }
+            } else {
+                sleep(1);
             }
         }
     }
